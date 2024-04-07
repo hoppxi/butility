f14396c1a2d7a9feeaf8ead03bd5d66ae4b1f209
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-04-07 08:14:01 +0300

    New enhanced import statements added.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,46 @@ import media from './media/index.js';
 import network from './network/index.js';
 import device from './device/index.js';
 
+// DOM modules
+import Element  from "./DOM/element/element.js";
+import Attribute  from "./DOM/element/attribute.js";
+import Obj  from "./DOM/composite/object.js";
+import String  from "./DOM/composite/string.js";
+import Color  from "./DOM/styling/color.js";
+import Style  from "./DOM/styling/style.js";
+import Utility  from "./DOM/main/utility.js";
+import DragDrop  from "./DOM/general/drag-drop.js";
+import Scroll  from "./DOM/general/scroll.js";
+import Modal  from "./DOM/modal/modal.js";
+import Ripple, { RippleEffect } from "./DOM/general/ripple.js";
+import Tooltip from "./DOM/general/tooltip.js";
+
+// form modules
+import FormAction, { SerializeForm } from "./form/main.js";
+import Validate from "./form/validate.js";
+import CreditCard from "./form/creditCard.js";
+
+// media modules
+import Image from "./media/device-media/image.js";
+import Capture from "./media/device-media/capture.js";
+import FullScreen from "./media/device-media/fullScreen.js";
+import QRCode from "./media/device-media/qrcode.js";
+import File from "./media/file/file.js";
+import Blob from "./media/file/blob.js";
+
+// network modules
+import URLClass from "./network/URL/url.js";
+import RequestServer from "./network/request/server.js";
+import IP from "./network/ip/ip.js";
+import WebWorker from "./network/web-worker/webWorker.js";
+
+// device modules
+import DetectFeature from "./device/detection/features.js";
+import DetectDevice from "./device/detection/device.js";
+import EnvInfo from "./device/detection/envInfo.js";
+import BrowserStorage from "./device/storage/browser-storage.js";
+import DeviceStorage from "./device/storage/device-storage.js";
+
 
 /**
  * Main module for the 'BrowserUtility' library, containing submodules for DOM, form, media, network, and device utilities.
@@ -79,8 +119,15 @@ const BrowserUtility = {
 }
 
 export default BrowserUtility;
+
 export { dom };
 export { form };
 export { media };
 export { network };
 export { device };
+
+export { Element, Attribute, Obj, String, Scroll, Utility, Ripple, RippleEffect, DragDrop, Style, Color, Modal, Tooltip };
+export { Validate, CreditCard, FormAction, SerializeForm };
+export { File, QRCode, Blob, Image, Capture, FullScreen };
+export { IP, RequestServer, URLClass, WebWorker };
+export { DetectDevice, DetectFeature, EnvInfo, DeviceStorage, BrowserStorage };
